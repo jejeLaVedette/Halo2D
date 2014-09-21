4b7c3edd46d1adfa2b4488075f9f6bd174333266
jejeLaVedette <jerome.barbier26@gmail.com>
2014-09-21 12:49:28 +0200

    ajout des premières créations : héro, méchant, début de map, gravité, sol, obstacles

---
+++ Halo2D/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Contrôleur du joueur
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+	/// <summary>
+	/// 1 - La vitesse de déplacement
+	/// </summary>
+	public Vector2 speed = new Vector2(10, 10);
+	
+	// 2 - Stockage du mouvement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Récupérer les informations du clavier/manette
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Calcul du mouvement
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * 0);
+		//movement = new Vector2(
+			//speed.x * inputX);
+		
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Déplacement
+		rigidbody2D.velocity = movement;
+	}
+}
