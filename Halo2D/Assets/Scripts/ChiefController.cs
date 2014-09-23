aa9906d1631e896bedc7c44fa5c4a84e0788b682
jejeLaVedette <jerome.barbier26@gmail.com>
2014-09-23 22:18:05 +0200

    ajout du saut (non illimité) + sprites modifié pour course + saut

--- Halo2D/Assets/Scripts/ChiefController.cs
+++ Halo2D/Assets/Scripts/ChiefController.cs
@@ -5,10 +5,23 @@ public class ChiefController : MonoBehaviour {
 
 	public Animator anim;
 	public float speed=8f;
+	public int jumpSpeed = 350;
+
+	//variable pour : tant que tu ne touches pas le seul : je ne saute pas 
+	public Transform checkSol;
+	bool toucheLeSol = false;
+	float rayonSol = 0.3f;
+	//defini : qu'est-ce que le sol
+	public LayerMask Sol;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+	}
+
+	void FixedUpdate(){
+		toucheLeSol = Physics2D.OverlapCircle (checkSol.position, rayonSol, Sol);
+		anim.SetBool ("Sol", toucheLeSol); 
 	}
 	
 	// Update is called once per frame
@@ -16,6 +29,11 @@ public class ChiefController : MonoBehaviour {
 		//on recup les touches clavier dédiées aux déplacement latéral
 		float x = Input.GetAxis("Horizontal");
 		anim.SetFloat ("speed", Mathf.Abs (x));
+
+		//gestion du saut
+		if (toucheLeSol && Input.GetButtonDown ("Jump")) {
+			rigidbody2D.AddForce(new Vector2(0,jumpSpeed));
+		}
 
 		//si je me déplace vers la droite
 		if (x > 0) {
